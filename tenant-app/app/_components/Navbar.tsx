33914cfadad1ad7a0a611e9fae64d4ec28032865
vstjal001 <vstjal001@cs.uct.ac.za>
2024-01-22 20:23:12 +0200

    Update Navbar styling to include full width

--- tenant-app/app/_components/Navbar.tsx
+++ tenant-app/app/_components/Navbar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 const Navbar = () => {
     return (
         <header
+            // className="fixed inset-x-0 top-0 z-30 mx-auto w-full border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl">
             className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
             <div className="px-4">
                 <div className="flex items-center justify-between">
