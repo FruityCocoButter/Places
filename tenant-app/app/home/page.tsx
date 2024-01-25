dfe4f9c394dd8adf7f052b6039176a6887bb7f64
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-01-25 22:33:10 +0200

    Added the nav bar with search

--- tenant-app/app/home/page.tsx
+++ tenant-app/app/home/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from '@/app/_components/Navbar'
+import SearchNav from '../_components/SearchNav'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        <Navbar />
+        <SearchNav/>
 
     </div>
   )
