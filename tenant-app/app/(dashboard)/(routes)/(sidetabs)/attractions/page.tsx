8d7bd3ca9a5d1f112e394ae3bb0361de48e6a46e
Kgaugelo Bopape <bopape9702@gmail.com>
2024-02-02 08:56:08 +0200

    Initial page navigation

--- tenant-app/app/(dashboard)/(routes)/(sidetabs)/attractions/page.tsx
+++ tenant-app/app/(dashboard)/(routes)/(sidetabs)/attractions/page.tsx
@@ -2,8 +2,15 @@ import AttractionCard from "../../../_components/attraction-card";
 
 const AttractionsPage = () => {
   return(
-      <div className="fixed inset-x-96 inset-y-56 h-screen w-1/2">
-        <AttractionCard images={["/armchair theatre 1.png", "/city rock 1.png", "/obs cafe 1.png"]}/>
+      <div className="fixed inset-x-[30rem] inset-y-56 h-screen w-1/2">
+        <div className="flex flex-row">
+                <AttractionCard images={["/armchair theatre 1.png", "/city rock 1.png", "/obs cafe 1.png"]}/>
+                <AttractionCard images={["/obs cafe 1.png" ,"/armchair theatre 1.png", "/city rock 1.png"]}/>
+            </div>
+            <div className="flex flex-row">
+              <AttractionCard images={[ "/city rock 1.png", "/obs cafe 1.png", "/armchair theatre 1.png"]}/>
+              <AttractionCard images={["/armchair theatre 1.png", "/city rock 1.png", "/obs cafe 1.png"]}/>
+            </div>
       </div>
   );
 }
--- tenant-app/app/(dashboard)/(routes)/(sidetabs)/properties/page.tsx
+++ tenant-app/app/(dashboard)/(routes)/(sidetabs)/properties/page.tsx
@@ -3,8 +3,16 @@ import PropertyCard from "../../../_components/property-card";
 
 const PropertiesPage = () => {
     return(
-        <div className="fixed inset-x-96 inset-y-56 h-screen w-1/2">
-            <PropertyCard images={["/mansion4.png", "/mansion5.png", "/mansion6.png"]}/>
+        <div className="fixed inset-x-[30rem] inset-y-56 h-screen w-1/2">
+            <div className="flex flex-row">
+                <PropertyCard images={["/mansion4.png", "/mansion5.png", "/mansion6.png"]}/>
+                <PropertyCard images={["/mansion5.png", "/mansion6.png", "/mansion4.png"]}/>
+            </div>
+            <div className="flex flex-row">
+                <PropertyCard images={["/mansion6.png", "/mansion4.png", "/mansion5.png"]}/>
+                <PropertyCard images={["/mansion4.png","/mansion5.png", "/mansion6.png"]}/>
+            </div>
+                
         </div>
     );
 }
