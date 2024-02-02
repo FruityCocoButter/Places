21496ed5aa4a1124d8e02b064e48a4d7f4f8c83b
Kgaugelo Bopape <bopape9702@gmail.com>
2024-02-02 22:11:25 +0200

    Added post and comment tab content and comment section content

--- tenant-app/app/(dashboard)/(routes)/(people)/[person]/comments/page.tsx
+++ tenant-app/app/(dashboard)/(routes)/(people)/[person]/comments/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { usePathname, useRouter } from 'next/navigation';
-import { cn } from "@/lib/utils";
+import CommentCard from '@/app/(dashboard)/_components/comment-card';
  
 const PersonComments = () => {
     const pathname = usePathname();
 
     return(
-        <div className="fixed inset-x-96 inset-y-36 m-4">
-            
-            <span>{pathname}</span>
+        <div className="fixed inset-x-[25rem] inset-y-[8rem] m-4">
+            <CommentCard/>
+            <CommentCard/>
+            <CommentCard/>
+            <CommentCard/>
         </div>
     )
 }
